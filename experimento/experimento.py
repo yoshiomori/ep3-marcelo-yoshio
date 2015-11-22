707f0d14e721d3cfce7ed04b9d4c96d2a47892e6
Yoshio Mori <yoshiomori90@gmail.com>
2015-11-22 01:36:09 -0200

    Atualizando o experimento

--- experimento/experimento.py
+++ experimento/experimento.py
@@ -1,38 +1,9 @@
+import math
+
 import ep3
-import os
 import sys
+import os
 from time import time
-
-
-# O sistemaVazio é o sistema de arquivos vazio
-# O sistema10MB é o o sistema de arquivos com 10MB ocupados
-# O sistema50MB é o sistema de arquivos com 50MB ocupados
-# A execução dessa função requer que os sistemas de arquivos: sistemaVazio, sistema10MB e sistema50MB tenham sido
-# devidamente inicializados
-
-
-# calcula o tempo de execução para a operação com os argumentos
-def tempo_op(op, args):
-    instante_inicial = time()
-    op(*args)
-    return time() - instante_inicial
-
-
-def experimento(nome_sistema):
-    tempo_cp = {'arquivo1MB': 0, 'arquivo10MB': 0, 'arquivo30MB': 0}
-    tempo_rm = {'arquivo1MB': 0, 'arquivo10MB': 0, 'arquivo30MB': 0}
-    ep3.mount(nome_sistema)
-    for nome_arquivo in ['arquivo1MB', 'arquivo10MB', 'arquivo30MB']:
-        try:
-            tempo_cp[nome_arquivo] = tempo_op(ep3.cp, (nome_arquivo, '/'))
-        except MemoryError:
-            pass
-    for nome_arquivo in ['arquivo1MB', 'arquivo10MB', 'arquivo30MB']:
-        tempo_rm[nome_arquivo] = tempo_op(ep3.rm, ('/' + nome_arquivo,))
-    tempo_rm_pai_sem_arquivos = tempo_op(ep3.rmdir, ('/pai_sem',))
-    tempo_rm_pai_com_arquivos = tempo_op(ep3.rmdir, ('/pai_com',))
-    ep3.umount()
-    return tempo_cp.values(), tempo_rm.values(), tempo_rm_pai_sem_arquivos, tempo_rm_pai_com_arquivos
 
 
 def cp(nome_origem, nome_destino):
@@ -43,68 +14,212 @@ def cp(nome_origem, nome_destino):
     origem.close()
 
 
-def esoma(a, b, c, d, e, f, g, h):
-    return [i + j for i, j in zip(a, e)], [i + j for i, j in zip(b, f)], c + g, d + h
+# calcula o tempo de execução para a operação com os argumentos
+def tempo_op(op, args):
+    instante_inicial = time()
+    op(*args)
+    return time() - instante_inicial
 
 
-def emin(a, b, c, d, e, f, g, h):
-    return [min(i, j) for i, j in zip(a, e)], [min(i, j) for i, j in zip(b, f)], min(c, g), min(d, h)
+def test_cp():
+    for arquivo, nome_octave, titulo in zip(['arquivo1MB', 'arquivo10MB', 'arquivo30MB'], ['cp1MB', 'cp10MB', 'cp30MB'],
+                                            ['Copia de 1MB', 'Copia de 10MB', 'Copia de 30MB']):
+        médias = []
+        menor = []
+        maior = []
+        mínimo = []
+        máximo = []
+
+        cp('arquivos/arquivo1MB', arquivo)
+        for nome_sistema in ['sistema_vazio', 'sistema_10MB', 'sistema_50MB']:
+            # Soma total
+            valores = []
+            mntcp = float('inf')
+            mxtcp = float('-inf')
+
+            for i in range(30):
+                print('iteração %d' % i)
+                cp('arquivos/' + nome_sistema, nome_sistema)
+                ep3.mount(nome_sistema)
+                t = tempo_op(ep3.cp, (arquivo, '/'))
+                ep3.umount()
+                valores.append(t)
+                mntcp = min(mntcp, t)
+                mxtcp = max(mxtcp, t)
+                os.remove(nome_sistema)
+            média = sum(valores) / 30
+            médias.append(média)
+            desvio = math.sqrt(sum([(x - média) ** 2 for x in valores]) / 30)
+            menor.append(média - (-1.96) * desvio / math.sqrt(30))
+            maior.append(média + (-1.96) * desvio / math.sqrt(30))
+            mínimo.append(mntcp)
+            máximo.append(mxtcp)
+        os.remove(arquivo)
+        f = open(nome_octave + '.m', 'w')
+        f.write('x = [0, 10, 50];\n')
+        f.write('media = %s;\n' % repr(médias))
+        f.write('menor = %s;\n' % repr(menor))
+        f.write('maior = %s;\n' % repr(maior))
+        f.write('minimo = %s;\n' % repr(mínimo))
+        f.write('maximo = %s;\n' % repr(máximo))
+        f.write("plot(x,media,';media;', x, menor, 'g;menor;', x, maior, 'g;maior;', x, minimo, 'r;minimo;', x, maximo,"
+                " 'r;maximo;')\n")
+        f.write("title('%s')\n" % titulo)
+        f.write("xlabel('Tamanho Ocupado do Sistema de Arquivo (MB)')\n")
+        f.write("ylabel('Tempo Gasto (ms)')\n")
+        f.close()
 
 
-def emax(a, b, c, d, e, f, g, h):
-    return [max(i, j) for i, j in zip(a, e)], [max(i, j) for i, j in zip(b, f)], max(c, g), max(d, h)
+def test_rm():
+    for arquivo, nome_octave, titulo in zip(['arquivo1MB', 'arquivo10MB', 'arquivo30MB'], ['rm1MB', 'rm10MB', 'rm30MB'],
+                                            ['Remocao de 1MB', 'Remocao de 10MB', 'Remocao de 30MB']):
+        médias = []
+        menor = []
+        maior = []
+        mínimo = []
+        máximo = []
+
+        cp('arquivos/arquivo1MB', arquivo)
+        for nome_sistema in ['sistema_vazio', 'sistema_10MB', 'sistema_50MB']:
+            # Soma total
+            valores = []
+            mntcp = float('inf')
+            mxtcp = float('-inf')
+
+            for i in range(30):
+                print('iteração %d' % i)
+                cp('arquivos/' + nome_sistema, nome_sistema)
+                ep3.mount(nome_sistema)
+                ep3.cp(arquivo, '/')
+                t = tempo_op(ep3.rm, (arquivo,))
+                ep3.umount()
+                valores.append(t)
+                mntcp = min(mntcp, t)
+                mxtcp = max(mxtcp, t)
+                os.remove(nome_sistema)
+            média = sum(valores) / 30
+            médias.append(média)
+            desvio = math.sqrt(sum([(x - média) ** 2 for x in valores]) / 30)
+            menor.append(média - (-1.96) * desvio / math.sqrt(30))
+            maior.append(média + (-1.96) * desvio / math.sqrt(30))
+            mínimo.append(mntcp)
+            máximo.append(mxtcp)
+        os.remove(arquivo)
+        f = open(nome_octave + '.m', 'w')
+        f.write('x = [0, 10, 50];\n')
+        f.write('media = %s;\n' % repr(médias))
+        f.write('menor = %s;\n' % repr(menor))
+        f.write('maior = %s;\n' % repr(maior))
+        f.write('minimo = %s;\n' % repr(mínimo))
+        f.write('maximo = %s;\n' % repr(máximo))
+        f.write("plot(x,media,';media;', x, menor, 'g;menor;', x, maior, 'g;maior;', x, minimo, 'r;minimo;', x, maximo,"
+                " 'r;maximo;')\n")
+        f.write("title('%s')\n" % titulo)
+        f.write("xlabel('Tamanho Ocupado do Sistema de Arquivo (MB)')\n")
+        f.write("ylabel('Tempo Gasto (ms)')\n")
+        f.close()
 
 
-def main():
-    # Soma total
-    stcp = [0, 0, 0]
-    strm = [0, 0, 0]
-    strmps = 0
-    strmpc = 0
+def test_rm_pai_sem():
+    médias = []
+    menor = []
+    maior = []
+    mínimo = []
+    máximo = []
+    for nome_sistema in ['sistema_vazio', 'sistema_10MB', 'sistema_50MB']:
+        # Soma total
+        valores = []
+        mntcp = float('inf')
+        mxtcp = float('-inf')
 
-    # Maximo
-    mxtcp = [float('-inf'), float('-inf'), float('-inf')]
-    mxtrm = [float('-inf'), float('-inf'), float('-inf')]
-    mxtrmps = float('-inf')
-    mxtrmpc = float('-inf')
+        for i in range(30):
+            print('iteração %d' % i)
+            cp('arquivos/' + nome_sistema, nome_sistema)
+            ep3.mount(nome_sistema)
+            # Criando o diretório pai com 30 níveis
+            for i in range(30):
+                ep3.mkdir('/pai/' + 'subdir/' * i)
+            t = tempo_op(ep3.rmdir, ('/pai',))
+            ep3.umount()
+            valores.append(t)
+            mntcp = min(mntcp, t)
+            mxtcp = max(mxtcp, t)
+            os.remove(nome_sistema)
+        média = sum(valores) / 30
+        médias.append(média)
+        desvio = math.sqrt(sum([(x - média) ** 2 for x in valores]) / 30)
+        menor.append(média - (-1.96) * desvio / math.sqrt(30))
+        maior.append(média + (-1.96) * desvio / math.sqrt(30))
+        mínimo.append(mntcp)
+        máximo.append(mxtcp)
+    f = open('rmpaisem.m', 'w')
+    f.write('x = [0, 10, 50];\n')
+    f.write('media = %s;\n' % repr(médias))
+    f.write('menor = %s;\n' % repr(menor))
+    f.write('maior = %s;\n' % repr(maior))
+    f.write('minimo = %s;\n' % repr(mínimo))
+    f.write('maximo = %s;\n' % repr(máximo))
+    f.write("plot(x,media,';media;', x, menor, 'g;menor;', x, maior, 'g;maior;', x, minimo, 'r;minimo;', x, maximo,"
+            " 'r;maximo;')\n")
+    f.write("title('Remocao do diretorio pai com 30 subdiretorios')\n")
+    f.write("xlabel('Tamanho Ocupado do Sistema de Arquivo (MB)')\n")
+    f.write("ylabel('Tempo Gasto (ms)')\n")
+    f.close()
 
-    # Mínimo
-    mntcp = [float('inf'), float('inf'), float('inf')]
-    mntrm = [float('inf'), float('inf'), float('inf')]
-    mntrmps = float('inf')
-    mntrmpc = float('inf')
 
-    cp('arquivos/arquivo1MB', 'arquivo1MB')
-    cp('arquivos/arquivo10MB', 'arquivo10MB')
-    cp('arquivos/arquivo30MB', 'arquivo30MB')
-    for i in range(30):
-        print('iteração %d' % i)
-        cp('arquivos/' + sys.argv[1], sys.argv[1])
-        a, b, c, d = experimento(sys.argv[1])
-        stcp, strm, strmps, strmpc = esoma(a, b, c, d, stcp, strm, strmps, strmpc)
-        mntcp, mntrm, mntrmps, mntrmpc = emin(a, b, c, d, mntcp, mntrm, mntrmps, mntrmpc)
-        mxtcp, mxtrm, mxtrmps, mxtrmpc = emax(a, b, c, d, mxtcp, mxtrm, mxtrmps, mxtrmpc)
-        os.remove(sys.argv[1])
-    os.remove('arquivo1MB')
-    os.remove('arquivo10MB')
-    os.remove('arquivo30MB')
-    file = open('experimento' + sys.argv[1], 'w')
-    file.write('soma dos tempos de cp: ' + str(stcp) + '\n')
-    file.write('soma dos tempos de rm: ' + str(strm) + '\n')
-    file.write('soma dos tempos de rm do diretório pai sem arquivos: ' + str(strmps) + '\n')
-    file.write('soma dos tempos de rm do diretório pai com arquivos: ' + str(strmpc) + '\n')
+def test_rm_pai_com():
+    médias = []
+    menor = []
+    maior = []
+    mínimo = []
+    máximo = []
+    for nome_sistema in ['sistema_vazio', 'sistema_10MB', 'sistema_50MB']:
+        # Soma total
+        valores = []
+        mntcp = float('inf')
+        mxtcp = float('-inf')
 
-    file.write('min dos tempos de cp: ' + str(mntcp) + '\n')
-    file.write('min dos tempos de rm: ' + str(mntrm) + '\n')
-    file.write('min dos tempos de rm do diretório pai sem arquivos: ' + str(mntrmps) + '\n')
-    file.write('min dos tempos de rm do diretório pai com arquivos: ' + str(mntrmpc) + '\n')
-
-    file.write('max dos tempos de cp: ' + str(mxtcp) + '\n')
-    file.write('max dos tempos de rm: ' + str(mxtrm) + '\n')
-    file.write('max dos tempos de rm do diretório pai sem arquivos: ' + str(mxtrmps) + '\n')
-    file.write('max dos tempos de rm do diretório pai com arquivos: ' + str(mxtrmpc) + '\n')
-    file.close()
+        for i in range(30):
+            print('iteração %d' % i)
+            cp('arquivos/' + nome_sistema, nome_sistema)
+            ep3.mount(nome_sistema)
+            # Criando o diretório pai com 30 níveis de hierarquia com centenas de arquivos regulares em todos os
+            # subdiretórios
+            for j in range(30):
+                caminho = '/pai/' + 'subdir/' * j
+                ep3.mkdir(caminho)
+                for k in range(100):
+                    ep3.touch(caminho + 'arquivo%d' % k)
+            t = tempo_op(ep3.rmdir, ('/pai',))
+            ep3.umount()
+            valores.append(t)
+            mntcp = min(mntcp, t)
+            mxtcp = max(mxtcp, t)
+            os.remove(nome_sistema)
+        média = sum(valores) / 30
+        médias.append(média)
+        desvio = math.sqrt(sum([(x - média) ** 2 for x in valores]) / 30)
+        menor.append(média - (-1.96) * desvio / math.sqrt(30))
+        maior.append(média + (-1.96) * desvio / math.sqrt(30))
+        mínimo.append(mntcp)
+        máximo.append(mxtcp)
+    f = open('rmpaicom.m', 'w')
+    f.write('x = [0, 10, 50];\n')
+    f.write('media = %s;\n' % repr(médias))
+    f.write('menor = %s;\n' % repr(menor))
+    f.write('maior = %s;\n' % repr(maior))
+    f.write('minimo = %s;\n' % repr(mínimo))
+    f.write('maximo = %s;\n' % repr(máximo))
+    f.write("plot(x,media,';media;', x, menor, 'g;menor;', x, maior, 'g;maior;', x, minimo, 'r;minimo;', x, maximo,"
+            " 'r;maximo;')\n")
+    f.write("title('Remocao do diretorio pai com 30 subdiretorios')\n")
+    f.write("xlabel('Tamanho Ocupado do Sistema de Arquivo (MB)')\n")
+    f.write("ylabel('Tempo Gasto (ms)')\n")
+    f.close()
 
 
 if __name__ == '__main__':
-    main()
+    test_cp()
+    test_rm()
+    test_rm_pai_sem()
+    test_rm_pai_com()
